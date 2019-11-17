931d40444bc96d881dfb09af41ae8359821f6c35
Hilton W. Silva <hws689@gmail.com>
2019-11-17 15:26:01 -0300

    Add promise

--- stocks/controllers/stocks.controller.js
+++ stocks/controllers/stocks.controller.js
@@ -22,17 +22,23 @@ module.exports = class StocksController {
             return cached;
         }
         let raioXData = {};
-        tabletojson.convertUrl(raioX, (tables) =>{
-            if(tables && tables[3]){
-                tables[3].forEach(element => {
-                    if(element['0'] === 'Lucro por Ação (LPA) $'){
-                        raioXData.lpaAvg = (parseFloat(element['1'].replace(',', '.')) + parseFloat(element['2'].replace(',', '.')) + parseFloat(element['3'].replace(',', '.'))) / 3
-                    }else if(element['0'] === 'Valor Patr Ação (VPA) $'){
-                        raioXData.vpaAvg = (parseFloat(element['1'].replace(',', '.')) + parseFloat(element['2'].replace(',', '.')) + parseFloat(element['3'].replace(',', '.'))) / 3
-                    }
-                });
-                cache.put(this.cacheKey + symbol, raioXData, this.cacheTime);
-            }
+        let promise = new Promise((resolve, reject) => {
+            tabletojson.convertUrl(raioX, (tables) =>{
+                if(tables && tables[3]){
+                    tables[3].forEach(element => {
+                        if(element['0'] === 'Lucro por Ação (LPA) $'){
+                            raioXData.lpaAvg = (parseFloat(element['1'].replace(',', '.')) + parseFloat(element['2'].replace(',', '.')) + parseFloat(element['3'].replace(',', '.'))) / 3
+                        }else if(element['0'] === 'Valor Patr Ação (VPA) $'){
+                            raioXData.vpaAvg = (parseFloat(element['1'].replace(',', '.')) + parseFloat(element['2'].replace(',', '.')) + parseFloat(element['3'].replace(',', '.'))) / 3
+                        }
+                    });
+                    cache.put(this.cacheKey + symbol, raioXData, this.cacheTime);
+                    resolve(raioXData);
+                }
+            });
         });
+
+        return await promise;
+        
     }
 }
