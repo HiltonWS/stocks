f11f98768de037cf0bf2f3423ef67139ad2a3e47
Hilton W. Silva <hilton.silva@qualirede.com.br>
2020-07-14 11:54:09 -0300

    Ajuste depedencias

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const routes = require('./stocks/routes');
-const tabletojson = require('tabletojson');
+const tabletojson = require('tabletojson').Tabletojson;
 const StocksController = require('./stocks/controllers/stocks.controller');
 const controller = new StocksController();
-
+const SSHConnection = require('node-ssh-forward').SSHConnection
 const app = express();
 const port = process.env.PORT || 5000;
 
 
 let init = async () => {
+  
   app.use(routes)
   app.listen(port, () =>
     console.log(`Stocks iniciado em ${port}!`),
